95190208d8ef366f6e64ccbe59c7a869c3484477
sidd-oo <sahoosiddharth3@gmail.com>
2022-02-08 08:08:34 +0530

    feat: Display of top five highscores

--- src/data/questionaire.js
+++ src/data/questionaire.js
@@ -44,74 +44,74 @@ export const questionaire = [
     },
     answer: "c",
   },
-  {
-    question_no: 4,
-    question: "What is a state in React?",
-    options: {
-      a: "A permanent storage.",
-      b: "Internal storage of the component.",
-      c: "External storage of the component.",
-      d: "None of the above.",
-    },
-    answer: "b",
-  },
-  {
-    question_no: 5,
-    question:
-      "Which of the following option is correct in the case of the Babel??",
-    options: {
-      a: "Babel is a Compiler.",
-      b: "Babel is a Transpiler",
-      c: "None of the above.",
-      d: "Both A and B are correct.",
-    },
-    answer: "d",
-  },
-  {
-    question_no: 6,
-    question: "What is ReactJS?",
-    options: {
-      a: "Server-side framework",
-      b: "User-interface framework",
-      c: "Both",
-      d: "None",
-    },
-    answer: "c",
-  },
-  {
-    question_no: 7,
-    question: "Props are __________ into other components",
-    options: {
-      a: "Injected",
-      b: "Methods",
-      c: "Both A and B",
-      d: "All of the these",
-    },
-    answer: "b",
-  },
-  {
-    question_no: 8,
-    question:
-      "What of the following is used in React.js to increase performance?",
-    options: {
-      a: "Original DOM",
-      b: "Virtual DOM",
-      c: "Both  A and B",
-      d: "None of the above.",
-    },
-    answer: "b",
-  },
-  {
-    question_no: 9,
-    question: "What is the default port where webpack-server runs?",
-    options: {
-      a: "3000",
-      b: "8000",
-      c: "8080",
-      d: "3030",
-    },
-    answer: "c",
-  },
+  // {
+  //   question_no: 4,
+  //   question: "What is a state in React?",
+  //   options: {
+  //     a: "A permanent storage.",
+  //     b: "Internal storage of the component.",
+  //     c: "External storage of the component.",
+  //     d: "None of the above.",
+  //   },
+  //   answer: "b",
+  // },
+  // {
+  //   question_no: 5,
+  //   question:
+  //     "Which of the following option is correct in the case of the Babel??",
+  //   options: {
+  //     a: "Babel is a Compiler.",
+  //     b: "Babel is a Transpiler",
+  //     c: "None of the above.",
+  //     d: "Both A and B are correct.",
+  //   },
+  //   answer: "d",
+  // },
+  // {
+  //   question_no: 6,
+  //   question: "What is ReactJS?",
+  //   options: {
+  //     a: "Server-side framework",
+  //     b: "User-interface framework",
+  //     c: "Both",
+  //     d: "None",
+  //   },
+  //   answer: "c",
+  // },
+  // {
+  //   question_no: 7,
+  //   question: "Props are __________ into other components",
+  //   options: {
+  //     a: "Injected",
+  //     b: "Methods",
+  //     c: "Both A and B",
+  //     d: "All of the these",
+  //   },
+  //   answer: "b",
+  // },
+  // {
+  //   question_no: 8,
+  //   question:
+  //     "What of the following is used in React.js to increase performance?",
+  //   options: {
+  //     a: "Original DOM",
+  //     b: "Virtual DOM",
+  //     c: "Both  A and B",
+  //     d: "None of the above.",
+  //   },
+  //   answer: "b",
+  // },
+  // {
+  //   question_no: 9,
+  //   question: "What is the default port where webpack-server runs?",
+  //   options: {
+  //     a: "3000",
+  //     b: "8000",
+  //     c: "8080",
+  //     d: "3030",
+  //   },
+  //   answer: "c",
+  // },
   //   {
   //     question_no: 10,
   //     question: "What is a state in React?",
--- src/pages/Highscore/Highscore.jsx
+++ src/pages/Highscore/Highscore.jsx
@@ -4,20 +4,22 @@ import Header from "../../components/header/Header";
 import "./Highscore.css";
 
 const Highscore = () => {
+  const scores = JSON.parse(localStorage.getItem("scores"));
   return (
     <div className="highscore">
       <Header />
       <section className="home-section">
         <h1>Highscores</h1>
         <ol>
-          <li>Siddharth - 43</li>
-          <li>Ibrahim - 38</li>
+          {scores && Object.keys(scores).sort((a, b)=> scores[b] - scores[a]).map((key, index) =>(
+            (index < 5) && <li key={index}>{key}  -  {scores[key]}</li>
+          ))}
         </ol>
         <div className="buttons">
           <Link to="/" className="link">
-            <button class="btn">Go Back</button>
+            <button className="btn">Go Back</button>
           </Link>
-          <button class="btn">Clear Highscore</button>
+          <button className="btn">Clear Highscore</button>
         </div>
       </section>
     </div>
