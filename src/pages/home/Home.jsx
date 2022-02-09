399a32b0af925a754c096e162c960b370bbb4b13
sidd-oo <sahoosiddharth3@gmail.com>
2022-02-09 23:42:24 +0530

    fix: Fixing the css for Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,20 +6,22 @@ const Home = () => {
   return (
     <div className="home">
       <Header />
-      <section className="home-section">
-        <h1>Coding Quiz Challenge</h1>
-        <p>
-          Try to answer the following code-related question within the time
-          limit.
-        </p>
-        <p>
-          Keep in mind that every incorrect answer will penalize your score/time
-          by ten seconds!
-        </p>
-        <Link to="quiz/0" className="link">
-          <button className="start-btn">Start Quiz</button>
-        </Link>
-      </section>
+      <main className="home-main">
+        <section className="home-section">
+          <h1 className="title">Coding Quiz Challenge</h1>
+          <p className="paragraph">
+            Try to answer the following code-related question within the time
+            limit.
+          </p>
+          <p className="paragraph">
+            Keep in mind that every incorrect answer will penalize your
+            score/time by ten seconds!
+          </p>
+          <Link to="quiz/0" className="link">
+            <button className="start-btn">Start Quiz</button>
+          </Link>
+        </section>
+      </main>
     </div>
   );
 };
